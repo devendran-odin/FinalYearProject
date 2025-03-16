323332c08d08032be833f93d79aaa9261d424abd
devendran-odin <devsanthosh720@gmail.com>
2025-03-16 18:36:29 +0530

    shadcn installed

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,10 @@
+import { Button } from "./components/ui/button";
+
 function App() {
   return (
     <>
       <h1 className="text-4xl text-green-500 bg-black">Start Coding</h1>
+      <Button variant="destructive">Cick me</Button>
     </>
   );
 }
